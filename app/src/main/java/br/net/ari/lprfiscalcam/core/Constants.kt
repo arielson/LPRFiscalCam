6c64377c66f4c8d0be6356f08d91e734e1d44065
arielson <arielsonf@hotmail.com>
2023-05-30 10:50:24 -0300

    Comentando constante

--- app/src/main/java/br/net/ari/lprfiscalcam/core/Constants.kt
+++ app/src/main/java/br/net/ari/lprfiscalcam/core/Constants.kt
@@ -3,7 +3,7 @@ package br.net.ari.lprfiscalcam.core
 class Constants {
     companion object {
         const val SamePlateDelay = 360
-        const val s = "c2VuaGExMjM3"
+//        const val s = "c2VuaGExMjM3"
         const val b = "YmluLmRhdGE="
         const val t = "dGVtcC5kYXRh"
         const val k = "5Qffcy8AghIWNYy2+Sh3uufDZAS2jL9JQRUqmEF4MAA="
